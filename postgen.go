f02cf593de8078cf21f5327ddcfe141a675addf5
Jerry Howard <jerryhoward@fastmail.com>
2016-07-04 00:26:28 -0400

    Now passing in the input and output filenames as args

--- postgen.go
+++ postgen.go
@@ -5,16 +5,28 @@ import (
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
-	input, err := ioutil.ReadFile("test.md")
+
+	args := os.Args[1:] // Reason: index 0 contains the program path
+	if len(args) != 2 {
+		fmt.Println("ERROR: Wrong number of arguments provided. We're expecting:")
+		fmt.Println("1. Input file")
+		fmt.Println("2. Output file")
+		os.Exit(1)
+	}
+	inputFile := args[0]
+	outputFile := args[1]
+
+	input, err := ioutil.ReadFile(inputFile)
 	check(err)
 
 	unsafe := blackfriday.MarkdownCommon(input)
 	safe := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
 
-	err = ioutil.WriteFile("test.html", safe, 0777)
+	err = ioutil.WriteFile(outputFile, safe, 0777)
 	check(err)
 
 	fmt.Println("Program finished, check result.")
