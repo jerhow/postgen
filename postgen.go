b8e61b6d16f5630b73fba0b498e399f99ea8b7b5
Jerry Howard <jerryhoward@fastmail.com>
2016-07-04 19:56:19 -0400

    Cleanup

--- postgen.go
+++ postgen.go
@@ -6,7 +6,6 @@ import (
 	"github.com/russross/blackfriday"
 	"io/ioutil"
 	"os"
-	"reflect"
 )
 
 func main() {
@@ -21,42 +20,25 @@ func main() {
 	inputFile := args[0]
 	outputFile := args[1]
 
-	input, err := ioutil.ReadFile(inputFile)
+	// Get the Markdown input
+	rawInput, err := ioutil.ReadFile(inputFile)
 	check(err)
 
-	unsafe := blackfriday.MarkdownCommon(input)
+	// Convert and sanitize our content
+	unsafe := blackfriday.MarkdownCommon(rawInput)
 	content := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
-
-	fmt.Println(reflect.TypeOf(content))
 
 	// Get the shared markup pieces
 	topHTML, err := ioutil.ReadFile("./shared_markup/top.html")
 	check(err)
-	fmt.Println(reflect.TypeOf(topHTML))
-	// fmt.Print(string(topHTML))
-	// fmt.Println(topHTML)
-
 	bottomHTML, err := ioutil.ReadFile("./shared_markup/bottom.html")
 	check(err)
-	fmt.Println(reflect.TypeOf(bottomHTML))
-	fmt.Println(len(bottomHTML))
 
-	fmt.Println("Trying our thing here...")
+	// Incrementally building a singular byte array via append()
 	finalOutput := append(topHTML[:], content[:]...)
 	finalOutput = append(finalOutput, bottomHTML[:]...)
-	// fmt.Println(string(testOutput))
-	// fmt.Println(append(topHTML[:], bottomHTML[:]...))
 
-	// capacity := len(topHTML) + len(content) + len(bottomHTML)
-	// fmt.Println(capacity)
-
-	// topHTML_str := string(topHTML)
-	// content_str := string(content)
-	// bottomHTML_str := string(bottomHTML)
-
-	// finalOutput_str := topHTML_str + content_str + bottomHTML_str
-	// finalOutput := []byte(finalOutput_str)
-
+	// Write our output to an HTML file
 	err = ioutil.WriteFile(outputFile, finalOutput, 0644)
 	check(err)
 
